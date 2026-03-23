dd9cd8f456737d68841eeb9a4a288ac936004092
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-03-23 12:11:32 +0000

    feat: add GET, POST routes for create, edit, delete games

--- routes/gamesRouter.js
+++ routes/gamesRouter.js
@@ -3,6 +3,12 @@ const gamesRouter = Router();
 const gamesControllers = require("../controllers/gamesControllers");
 
 gamesRouter.get("/", gamesControllers.getAllGames);
+gamesRouter.get("/createGame", gamesControllers.getCreateGame);
 gamesRouter.get("/:gameId", gamesControllers.getGameById);
+gamesRouter.get("/edit/:gameId", gamesControllers.getUpdateGameById);
+
+gamesRouter.post("/createGame", gamesControllers.gameValidators, gamesControllers.createGame);
+gamesRouter.post("/edit/:gameId", gamesControllers.gameValidators, gamesControllers.updateGame);
+gamesRouter.post("/delete/:gameId", gamesControllers.deleteGame);
 
 module.exports = gamesRouter;
