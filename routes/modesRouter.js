cd76a5918c771afdfa77ee1b31ddbef137ec3749
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-03-24 15:56:53 +0000

    feat: add GET, POST routes for create, edit, delete modes

--- routes/modesRouter.js
+++ routes/modesRouter.js
@@ -3,6 +3,12 @@ const modesRouter = Router();
 const modesControllers = require("../controllers/modesControllers");
 
 modesRouter.get("/", modesControllers.getAllModes);
+modesRouter.get("/create", modesControllers.getCreateMode);
 modesRouter.get("/:modeId", modesControllers.getGamesByModeId);
+modesRouter.get("/edit/:modeId", modesControllers.getUpdateModeById);
+
+modesRouter.post("/create", modesControllers.modesValidators, modesControllers.createMode);
+modesRouter.post("/edit/:modeId", modesControllers.modesValidators, modesControllers.updateMode);
+modesRouter.post("/delete/:modeId", modesControllers.deleteMode);
 
 module.exports = modesRouter;
