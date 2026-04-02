ac1858989b58a63dd8fea02ad10962669a867ec5
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-04-02 15:48:28 +0100

    refactor: update developer names to links for gamesList
    - update developer names to links to take users to dedicated developer pages
    - update getAllGames database query to include developer id column required for links

--- db/queries/gamesQueries.js
+++ db/queries/gamesQueries.js
@@ -25,6 +25,7 @@ async function getAllGames() {
       ga.id AS game_id,
       ga.name AS game_name,
       ga.release_year,
+      de.id AS developer_id,
       de.name AS developer_name,
       gen.genres,
       mo.modes,
